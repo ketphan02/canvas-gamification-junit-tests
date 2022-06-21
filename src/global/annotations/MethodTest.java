e5d874a087c3a4c4a2f08a1fcdad5dc3099204e8
Hedgemon4 <sethlinc4@gmail.com>
2022-06-21 17:35:05 -0600

    Added TODO to MethodTest

--- src/global/annotations/MethodTest.java
+++ src/global/annotations/MethodTest.java
@@ -3,7 +3,7 @@ package global.annotations;
 
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
-
+// TODO: Finish implementation so MethodTest sets up input and output for a method being tested
 @Retention(RetentionPolicy.RUNTIME)
 public @interface  MethodTest {
     String value() default "";
