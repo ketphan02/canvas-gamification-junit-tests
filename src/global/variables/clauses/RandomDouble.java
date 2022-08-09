e69dac41320df7e1684a0efbacf2bc9cec093159
Restyled.io <commits@restyled.io>
2022-08-09 23:26:56 +0000

    Restyled by clang-format

--- src/global/variables/clauses/RandomDouble.java
+++ src/global/variables/clauses/RandomDouble.java
@@ -1,123 +1,122 @@
 package global.variables.clauses;
 
-import global.utils.RandomUtil;
-import global.utils.RegexUtil;
-import global.variables.Clause;
-import global.variables.RandomClause;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.awaitility.Awaitility;
-import org.awaitility.Durations;
-
 import static global.tools.CustomAssertions.assertWithinRange;
 import static global.utils.RandomUtil.frequenciesAreRandom;
 import static global.utils.RegexUtil.orNegative;
 
+import global.utils.RandomUtil;
+import global.utils.RegexUtil;
+import global.variables.Clause;
+import global.variables.RandomClause;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import org.awaitility.Awaitility;
+import org.awaitility.Durations;
+
 public class RandomDouble extends Clause implements RandomClause<Double> {
-    static Map<Integer, ArrayList<Double>> valueStore = new HashMap<>();
-    private final double lower, upper;
-    private int precision = 16;  // max double precision
+  static Map<Integer, ArrayList<Double>> valueStore = new HashMap<>();
+  private final double lower, upper;
+  private int precision = 16; // max double precision
 
-    public RandomDouble(double lower, double upper) {
-        super();
-        this.lower = lower;
-        this.upper = upper;
+  public RandomDouble(double lower, double upper) {
+    super();
+    this.lower = lower;
+    this.upper = upper;
+  }
+
+  public RandomDouble(double lower, double upper, String name) {
+    super(name);
+    this.lower = lower;
+    this.upper = upper;
+  }
+
+  public RandomDouble(int lower, int upper) {
+    this((double)lower, upper);
+    // needs one value to be explicitly casted otherwise it calls the same
+    // constructor recursively
+  }
+
+  public RandomDouble(int lower, int upper, String name) {
+    this((double)lower, upper, name);
+  }
+
+  public RandomDouble(double lower, double upper, int precision) {
+    super();
+    this.lower = lower;
+    this.upper = upper;
+    this.precision = precision;
+  }
+
+  public RandomDouble(double lower, double upper, int precision, String name) {
+    super(name);
+    this.lower = lower;
+    this.upper = upper;
+    this.precision = precision;
+  }
+
+  public void trackValue(int matchGroupNum, String matchGroupValue) {
+    valueStore.computeIfAbsent(matchGroupNum, k -> new ArrayList<>());
+    (valueStore.get(matchGroupNum)).add(convertFromRegexGroup(matchGroupValue));
+  }
+
+  public boolean validateRandom(int matchGroupNum) {
+    Awaitility.await()
+        .pollDelay(Durations.TWO_HUNDRED_MILLISECONDS)
+        .until(() -> true);
+    if (valueStore.get(matchGroupNum) == null)
+      return false;
+    ArrayList<Double> values = valueStore.get(matchGroupNum);
+    final int numBins = RandomDouble.getNumBins(lower, upper);
+
+    int[] observedCounts = new int[numBins];
+    for (double value : values) {
+      int binNum = RandomDouble.assignedBinIndex(value, lower, upper, numBins);
+      if (binNum == RandomUtil.NO_BIN)
+        return false;
+      observedCounts[binNum]++;
     }
 
-    public RandomDouble(double lower, double upper, String name) {
-        super(name);
-        this.lower = lower;
-        this.upper = upper;
-    }
+    return frequenciesAreRandom(observedCounts, numBins);
+  }
 
-    public RandomDouble(int lower, int upper) {
-        this((double) lower, upper);
-        //needs one value to be explicitly casted otherwise it calls the same constructor recursively
-    }
+  private static int getNumBins(double lower, double upper) {
+    int upperCeil = (int)(upper + 1);
+    int lowerFloor = (int)lower;
+    return RandomUtil.getNumBins(lowerFloor, upperCeil); // TODO: do properly
+  }
 
-    public RandomDouble(int lower, int upper, String name) {
-        this((double) lower, upper, name);
-    }
+  private static int assignedBinIndex(double value, double lower, double upper,
+                                      int numBins) {
+    double gap = (upper - lower) / numBins;
+    assertWithinRange(
+        value, lower, upper,
+        "One or more of your randomly generated numbers fall outside of the required range.");
+    int binNumber = (int)((value - lower) / gap);
+    return (binNumber <= numBins) ? binNumber : RandomUtil.NO_BIN;
+  }
 
-    public RandomDouble(double lower, double upper, int precision) {
-        super();
-        this.lower = lower;
-        this.upper = upper;
-        this.precision = precision;
-    }
+  public double getLower() { return lower; }
 
-    public RandomDouble(double lower, double upper, int precision, String name) {
-        super(name);
-        this.lower = lower;
-        this.upper = upper;
-        this.precision = precision;
-    }
+  public double getUpper() { return upper; }
 
-    public void trackValue(int matchGroupNum, String matchGroupValue) {
-        valueStore.computeIfAbsent(matchGroupNum, k -> new ArrayList<>());
-        (valueStore.get(matchGroupNum)).add(convertFromRegexGroup(matchGroupValue));
-    }
+  public double getPrecision() { return precision; }
 
-    public boolean validateRandom(int matchGroupNum) {
-        Awaitility.await().pollDelay(Durations.TWO_HUNDRED_MILLISECONDS).until(() -> true);
-        if (valueStore.get(matchGroupNum) == null)
-            return false;
-        ArrayList<Double> values = valueStore.get(matchGroupNum);
-        final int numBins = RandomDouble.getNumBins(lower, upper);
+  public Double convertFromRegexGroup(String matchGroupString) {
+    // TODO: try catch
+    return Double.parseDouble(matchGroupString);
+  }
 
-        int[] observedCounts = new int[numBins];
-        for (double value : values) {
-            int binNum = RandomDouble.assignedBinIndex(value, lower, upper, numBins);
-            if (binNum == RandomUtil.NO_BIN) return false;
-            observedCounts[binNum]++;
-        }
+  @Override
+  public ArrayList<Double> getValuesForMatchGroup(int matchGroup) {
+    return valueStore.get(matchGroup);
+  }
 
-        return frequenciesAreRandom(observedCounts, numBins);
-    }
-
-    private static int getNumBins(double lower, double upper) {
-        int upperCeil = (int) (upper + 1);
-        int lowerFloor = (int) lower;
-        return RandomUtil.getNumBins(lowerFloor, upperCeil); // TODO: do properly
-    }
-
-    private static int assignedBinIndex(double value, double lower, double upper, int numBins) {
-        double gap = (upper - lower) / numBins;
-        assertWithinRange(value, lower, upper, "One or more of your randomly generated numbers fall outside of the required range.");
-        int binNumber = (int) ((value - lower) / gap);
-        return (binNumber <= numBins) ? binNumber : RandomUtil.NO_BIN;
-    }
-
-    public double getLower() {
-        return lower;
-    }
-
-    public double getUpper() {
-        return upper;
-    }
-
-    public double getPrecision() {
-        return precision;
-    }
-
-    public Double convertFromRegexGroup(String matchGroupString) {
-        // TODO: try catch
-        return Double.parseDouble(matchGroupString);
-    }
-
-    @Override
-    public ArrayList<Double> getValuesForMatchGroup(int matchGroup) {
-        return valueStore.get(matchGroup);
-    }
-
-    @Override
-    public String getRegex() {
-        int lowSplit = RegexUtil.getSplitDecimal(lower)[0];
-        int highSplit = RegexUtil.getSplitDecimal(upper)[0];
-        String regexContent = RegexUtil.getRegexInt(lowSplit, highSplit);
-        return "(" + orNegative(regexContent + "\\.\\d+") + ")";
-    }
+  @Override
+  public String getRegex() {
+    int lowSplit = RegexUtil.getSplitDecimal(lower)[0];
+    int highSplit = RegexUtil.getSplitDecimal(upper)[0];
+    String regexContent = RegexUtil.getRegexInt(lowSplit, highSplit);
+    return "(" + orNegative(regexContent + "\\.\\d+") + ")";
+  }
 }
--- src/global/variables/clauses/RandomInteger.java
+++ src/global/variables/clauses/RandomInteger.java
@@ -1,76 +1,73 @@
 package global.variables.clauses;
 
-import global.utils.RegexUtil;
-import global.variables.Clause;
-import global.variables.RandomClause;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-
-import org.awaitility.Awaitility;
-import org.awaitility.Durations;
-
 import static global.utils.RandomUtil.*;
 import static global.utils.RegexUtil.orNegative;
 
+import global.utils.RegexUtil;
+import global.variables.Clause;
+import global.variables.RandomClause;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import org.awaitility.Awaitility;
+import org.awaitility.Durations;
+
 public class RandomInteger extends Clause implements RandomClause<Integer> {
-    static Map<Integer, ArrayList<Integer>> valueStore = new HashMap<>();
-    private final int lower, upper;
+  static Map<Integer, ArrayList<Integer>> valueStore = new HashMap<>();
+  private final int lower, upper;
 
-    public RandomInteger(int lower, int upper) {
-        super();
-        this.lower = lower;
-        this.upper = upper;
-    }
+  public RandomInteger(int lower, int upper) {
+    super();
+    this.lower = lower;
+    this.upper = upper;
+  }
 
-    public RandomInteger(int lower, int upper, String name) {
-        super(name);
-        this.lower = lower;
-        this.upper = upper;
-    }
+  public RandomInteger(int lower, int upper, String name) {
+    super(name);
+    this.lower = lower;
+    this.upper = upper;
+  }
 
-    public void trackValue(int matchGroupNum, String matchGroupValue) {
-        valueStore.computeIfAbsent(matchGroupNum, k -> new ArrayList<>());
-        (valueStore.get(matchGroupNum)).add(convertFromRegexGroup(matchGroupValue));
-    }
+  public void trackValue(int matchGroupNum, String matchGroupValue) {
+    valueStore.computeIfAbsent(matchGroupNum, k -> new ArrayList<>());
+    (valueStore.get(matchGroupNum)).add(convertFromRegexGroup(matchGroupValue));
+  }
 
-    public boolean validateRandom(int matchGroupNum) {
-        Awaitility.await().pollDelay(Durations.TWO_HUNDRED_MILLISECONDS).until(() -> true);
-        if (valueStore.get(matchGroupNum) == null)
-            return false;
-        ArrayList<Integer> values = valueStore.get(matchGroupNum);
-        final int NUM_BINS = getNumBins(lower, upper);
-        int[] observedCounts = new int[NUM_BINS];
-        for (int value : values) {
-            int binNum = assignedBinIndex(value, lower, upper, NUM_BINS);
-            if (binNum == NO_BIN) return false;
-            observedCounts[binNum]++;
-        }
-        return frequenciesAreRandom(observedCounts, NUM_BINS);
+  public boolean validateRandom(int matchGroupNum) {
+    Awaitility.await()
+        .pollDelay(Durations.TWO_HUNDRED_MILLISECONDS)
+        .until(() -> true);
+    if (valueStore.get(matchGroupNum) == null)
+      return false;
+    ArrayList<Integer> values = valueStore.get(matchGroupNum);
+    final int NUM_BINS = getNumBins(lower, upper);
+    int[] observedCounts = new int[NUM_BINS];
+    for (int value : values) {
+      int binNum = assignedBinIndex(value, lower, upper, NUM_BINS);
+      if (binNum == NO_BIN)
+        return false;
+      observedCounts[binNum]++;
     }
+    return frequenciesAreRandom(observedCounts, NUM_BINS);
+  }
 
-    public int getUpper() {
-        return upper;
-    }
+  public int getUpper() { return upper; }
 
-    public int getLower() {
-        return lower;
-    }
+  public int getLower() { return lower; }
 
-    public Integer convertFromRegexGroup(String matchGroupString) {
-        // TODO: try catch
-        return Integer.parseInt(matchGroupString);
-    }
+  public Integer convertFromRegexGroup(String matchGroupString) {
+    // TODO: try catch
+    return Integer.parseInt(matchGroupString);
+  }
 
-    @Override
-    public ArrayList<Integer> getValuesForMatchGroup(int matchGroup) {
-        return valueStore.get(matchGroup);
-    }
+  @Override
+  public ArrayList<Integer> getValuesForMatchGroup(int matchGroup) {
+    return valueStore.get(matchGroup);
+  }
 
-    @Override
-    public String getRegex() {
-        String regexContent = RegexUtil.getRegexInt(getLower(), getUpper());
-        return "(" + orNegative(regexContent) + ")";
-    }
+  @Override
+  public String getRegex() {
+    String regexContent = RegexUtil.getRegexInt(getLower(), getUpper());
+    return "(" + orNegative(regexContent) + ")";
+  }
 }
